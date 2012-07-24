b5f27d818a72062f624e120ddc8286d5bff9d4c3
mpage <mpage@rbcon.com>
2012-07-24 14:54:04 -0700

    Add tool to convert json formatted logs into more human-friendly format
    Change-Id: Id82ba72cdc47d08e6f14508f28909483b2854a54

---
+++ lib/steno/json_prettifier.rb
@@ -0,0 +1,66 @@
+require "digest/md5"
+require "yajl"
+
+module Steno
+end
+
+# Transforms JSON log lines into a more human readable format
+class Steno::JsonPrettifier
+
+  attr_reader :time_format
+
+  def initialize
+    @time_format = "%Y-%m-%d %H:%M:%S.%6N"
+  end
+
+  def prettify_line(line)
+    json_record = Yajl::Parser.parse(line)
+
+    format_record(json_record)
+  end
+
+  private
+
+  def format_record(record)
+    timestamp = nil
+    if record.has_key?("timestamp")
+      timestamp = Time.at(record["timestamp"]).strftime(@time_format)
+    else
+      timestamp = "-"
+    end
+
+    log_level = nil
+    if record.has_key?("log_level")
+      log_level = record["log_level"].upcase
+    else
+      log_level = "-"
+    end
+
+    fields = [timestamp,
+              record["source"] || "-",
+              "pid=%s" % [record["process_id"] || "-"],
+              "tid=%s" % [shortid(record["thread_id"])],
+              "fid=%s" % [shortid(record["fiber_id"])],
+              "%s/%s:%s" % [record["file"]   || "-",
+                            record["method"] || "-",
+                            record["lineno"] || "-"],
+              format_data(record["data"]),
+              "%7s" % [log_level],
+              "--",
+              record["message"] || "-"]
+
+    fields.join(" ") + "\n"
+  end
+
+  def format_data(data = {})
+    return "-" if data.empty?
+
+    data.map { |k, v| "#{k}=#{v}" }.join(",")
+  end
+
+  def shortid(data)
+    return "-" if data.nil?
+    digest = Digest::MD5.hexdigest(data.to_s)
+    digest[0, 4]
+  end
+end
