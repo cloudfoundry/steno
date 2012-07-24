ab94455dae99960e1e772a785b54ac9c93e4aabc
mpage <mpage@rbcon.com>
2012-07-24 14:54:04 -0700

    Add tool to convert json formatted logs into more human-friendly format
    Change-Id: Id82ba72cdc47d08e6f14508f28909483b2854a54

---
+++ spec/unit/json_prettifier_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+require "steno/json_prettifier"
+
+describe Steno::JsonPrettifier do
+  let(:prettifier) { Steno::JsonPrettifier.new }
+  let(:codec) { Steno::Codec::Json.new }
+
+  describe "#prettify_line" do
+    it "should return a properly formatted string" do
+      record = Steno::Record.new("test", :info, "message",
+                                 ["filename", "line", "method"], "test" => "data")
+      encoded = codec.encode_record(record)
+      prettified = prettifier.prettify_line(encoded)
+
+      exp_regex = ['\d{4}-\d{2}-\d{2}',        # YYYY-MM-DD
+                   '\d{2}:\d{2}:\d{2}\.\d{6}', # HH:MM:SS.uS
+                   'test',                     # Source
+                   'pid=\d+',                  # Process id
+                   'tid=\w{4}',                # Thread shortid
+                   'fid=\w{4}',                # Fiber shortid
+                   'filename\/method:line',    # Location
+                   'test=data',                # User supplied data
+                   '\s+INFO',                  # Level
+                   '--',
+                   'message',                  # Log message
+                   ].join(" ") + "\n"
+      prettified.should match(exp_regex)
+    end
+  end
+end
