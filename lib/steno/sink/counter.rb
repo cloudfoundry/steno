619b634bd2ba414eac016e3dffd0a80bb3aea84e
David Sabeti and Jeff Schnitzer <pair+dsabeti+jschnitzer@pivotallabs.com>
2013-06-05 14:05:30 -0700

    Add counter sink

---
+++ lib/steno/sink/counter.rb
@@ -0,0 +1,44 @@
+require "steno/sink/base"
+
+module Steno
+  module Sink
+  end
+end
+
+class Steno::Sink::Counter < Steno::Sink::Base
+
+  def initialize
+    # Map of String -> numeric count
+    @counts = {}
+    @mutex = Mutex.new
+  end
+
+  def add_record(record)
+    level = record.log_level.name.to_s
+
+    @mutex.synchronize do
+      unless @counts[level]
+        @counts[level] = 0
+      end
+      @counts[level] += 1
+    end
+  end
+
+  def flush
+  end
+
+  def to_json
+    hash = {}
+    @mutex.synchronize do
+      Steno::Logger::LEVELS.keys.each do |level_name|
+        hash[level_name] = @counts.fetch(level_name.to_s, 0)
+      end
+    end
+    Yajl::Encoder.encode(hash)
+  end
+
+  # Provide a map of string level -> count. This is thread-safe, the return value is a copy.
+  def counts
+    @mutex.synchronize { @counts.dup }
+  end
+end
--- lib/steno/version.rb
+++ lib/steno/version.rb
@@ -1,3 +1,3 @@
 module Steno
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 end
