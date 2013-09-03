e9e4c0cc941492538d7acd4dd3aad8333fe29a2d
Tier 3 <github@tier3.com>
2013-09-03 15:21:59 -0700

    Add code and tests to work on windows and unix.

--- lib/steno/sink/syslog.rb
+++ lib/steno/sink/syslog.rb
@@ -1,56 +1,59 @@
-require "steno/sink/base"
+unless Steno::Sink::WINDOWS
+  require "steno/sink/base"
 
-require "singleton"
-require "thread"
-require "syslog"
+  require "singleton"
+  require "thread"
+  require "syslog"
 
-class Steno::Sink::Syslog < Steno::Sink::Base
-  include Singleton
+  class Steno::Sink::Syslog < Steno::Sink::Base
+    include Singleton
 
-  MAX_MESSAGE_SIZE = 1024 * 3
+    MAX_MESSAGE_SIZE = 1024 * 3
 
-  LOG_LEVEL_MAP = {
-      :fatal  => Syslog::LOG_CRIT,
-      :error  => Syslog::LOG_ERR,
-      :warn   => Syslog::LOG_WARNING,
-      :info   => Syslog::LOG_INFO,
-      :debug  => Syslog::LOG_DEBUG,
-      :debug1 => Syslog::LOG_DEBUG,
-      :debug2 => Syslog::LOG_DEBUG,
+    LOG_LEVEL_MAP = {
+        :fatal  => Syslog::LOG_CRIT,
+        :error  => Syslog::LOG_ERR,
+        :warn   => Syslog::LOG_WARNING,
+        :info   => Syslog::LOG_INFO,
+        :debug  => Syslog::LOG_DEBUG,
+        :debug1 => Syslog::LOG_DEBUG,
+        :debug2 => Syslog::LOG_DEBUG,
     }
 
-  def initialize
-    super
+    def initialize
+      super
 
-    @syslog = nil
-    @syslog_lock = Mutex.new
-  end
+      @syslog = nil
+      @syslog_lock = Mutex.new
+    end
 
-  def open(identity)
-    @identity = identity
-    @syslog = Syslog.open(@identity, Syslog::LOG_PID, Syslog::LOG_USER)
-  end
+    def open(identity)
+      @identity = identity
+      @syslog = Syslog.open(@identity, Syslog::LOG_PID, Syslog::LOG_USER)
+    end
 
-  def add_record(record)
-    record = truncate_record(record)
-    msg = @codec.encode_record(record)
-    pri = LOG_LEVEL_MAP[record.log_level]
-    @syslog_lock.synchronize { @syslog.log(pri, "%s", msg) }
-  end
+    def add_record(record)
+      record = truncate_record(record)
+      msg = @codec.encode_record(record)
+      pri = LOG_LEVEL_MAP[record.log_level]
+      @syslog_lock.synchronize { @syslog.log(pri, "%s", msg) }
+    end
 
-  def flush
-    nil
-  end
-  private
+    def flush
+      nil
+    end
 
-  def truncate_record(record)
-    return record if record.message.size <= MAX_MESSAGE_SIZE
+    private
 
-    truncated = record.message.slice(0..(MAX_MESSAGE_SIZE - 1))
-    truncated << "..."
-    Steno::Record.new(record.source, record.log_level,
-                      truncated,
-                      [record.file, record.lineno, record.method],
-                      record.data)
+    def truncate_record(record)
+      return record if record.message.size <= MAX_MESSAGE_SIZE
+
+      truncated = record.message.slice(0..(MAX_MESSAGE_SIZE - 1))
+      truncated << "..."
+      Steno::Record.new(record.source, record.log_level,
+                        truncated,
+                        [record.file, record.lineno, record.method],
+                        record.data)
+    end
   end
 end
